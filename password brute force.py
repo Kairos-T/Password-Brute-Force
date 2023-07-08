6131f2883c230eaf52527ba7c721786d577ef058
Kairos Tay <80029462+Kairos-T@users.noreply.github.com>
2023-07-08 20:34:27 +0800

    Update password brute force.py

--- password brute force.py
+++ password brute force.py
@@ -1,41 +1,55 @@
 import itertools
 import time
 import sys
+from multiprocessing import Pool, cpu_count
 
-def crack_password(password, length, max_time):
+def crack_password(password):
     chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-    start_time = time.time()
+    max_length = 10
 
-    for n in range(1, length+1):
+    for n in range(1, max_length+1):
         for test in itertools.product(chars, repeat=n):
             test = "".join(test)
             if test == password:
                 return test
 
-            elapsed_time = time.time() - start_time
-            if elapsed_time > max_time:
-                return None
-
-            progress = n / length * 100
-            sys.stdout.write('\r')
-            sys.stdout.write("[%-100s] %d%%" % ('=' * int(progress), progress))
-            sys.stdout.flush()
-
     return None
 
+def progress_report(progress):
+    sys.stdout.write('\r')
+    sys.stdout.write("[%-100s] %d%%" % ('=' * int(progress), progress))
+    sys.stdout.flush()
 
 def main():
     password = input("Enter a password: ")
-    length = 10
     max_time = 60
 
-    cracked_password = crack_password(password, length, max_time)
+    pool = Pool(cpu_count())
+    length_range = range(1, 11)
 
-    if cracked_password:
-        print("\nPassword cracked! The password you entered is:", cracked_password)
-    else:
-        print("\nPassword not found within the time limit.")
+    start_time = time.time()
+    results = []
+    for length in length_range:
+        result = pool.apply_async(crack_password, (password,))
+        results.append(result)
 
+        elapsed_time = time.time() - start_time
+        if elapsed_time > max_time:
+            break
+
+        progress = length / len(length_range) * 100
+        progress_report(progress)
+
+    pool.close()
+    pool.join()
+
+    for result in results:
+        cracked_password = result.get()
+        if cracked_password:
+            print("\nPassword cracked! The password you entered is:", cracked_password)
+            return
+
+    print("\nPassword not found within the time limit.")
 
 if __name__ == "__main__":
     main()
